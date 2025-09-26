05f97ad6823fa56ff20de44247c6b2065fad5505
Voyane Rogerio <voyanerogerio@hotmail.com>
2025-09-26 09:25:10 +0200

    Modificado o ficheiro app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,10 +9,30 @@ db = SQLAlchemy(app)
 #Modelagem
 #Produto (id, name, price, description)
 class Product(db.Model):
-    id = db.Column(db.Integer, prymary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120), nullable = False)
     price = db.Column(db.Float, nullable = False)
     description = db.Column(db.Text, nullable = True)
+
+@app.route('/api/products/add', methods = ["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name = data["name"], price = data["price"], description = data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({'message': "Product added successfully!"})
+    return jsonify({'message': "Invalid product data!"}), 400
+
+@app.route('/api/products/delete/<int:product_id>', methods = ["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'message': "Product deleted successfully!"})
+    return jsonify({'message': "Product not found!"}), 404
+
 
 
 #Definir uma rota raiz(pagina inicial) e a função que será excutada ao requisitar
@@ -21,4 +41,6 @@ def hello_world():
     return "Hello World!"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
